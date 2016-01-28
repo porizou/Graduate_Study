cae1e6941ce85259eca2a3343d4295ee1d677fea
porizou <kourobo.hnnct@gmail.com>
2016-01-28 21:56:11 +0900

    change main

--- Wheelchair_Sonar/main.cpp
+++ Wheelchair_Sonar/main.cpp
@@ -5,71 +5,25 @@ DigitalOut led(LED1, 0);
 Serial pc(dp16, dp15);
 I2CSlave slave(dp5 , dp27);
 
-Ping* ping[2] = {
-	new Ping(dp17), //front
-	new Ping(dp11)  //back
-};
-
-char distance[2] = {0};
-int status;
+Ping pings[2] = {Ping(dp17), Ping(dp6)};
 
 int main() {
 
-	slave.address(0xA0);
+    slave.address(0xA0);
+    char distance[2] = {};
+    int i = 0;
+    int status;
 
-	while(1) {
+    while(1) {
 
-		do{status = slave.receive();}while(!(status == I2CSlave::ReadAddressed));
+        do{status = slave.receive();}while(!(status == I2CSlave::ReadAddressed));
 
-		slave.write(distance, 2); led = 1;
+        slave.write(distance, 2); led = !led;
 
-		pc.printf("front:%d,back:%d\r\n",distance[0],distance[1]);
+        pings[i%2].Send();
+        wait_ms(30);
+        distance[i%2] = (char)pings[i%2].Read_cm() / 2;
 
-		for (int i = 0; i < 2; i++) {
-			ping[i]->Send();
-			wait_ms(30);
-			distance[i] = (char)ping[i]->Read_cm() / 2;
-		}
-	}
+        i++;
+    }
 }
-
-#include "mbed.h"
-#include "Ping.h"
-
-DigitalOut led(LED1, 0);
-Serial pc(dp16, dp15);
-I2CSlave slave(dp5 , dp27);
-
-Ping ping(dp11);
-int status;
-
-int main() {
-
-	slave.address(0xA0);
-
-	while(1) {
-
-		do{status = slave.receive();}while(!(status == I2CSlave::ReadAddressed));
-
-		slave.write(distance, 2); led = 1;
-
-		ping.Send();
-		wait_ms(30);
-
-		distance[0] = (char)ping.Read_cm() / 2; 
-	}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
